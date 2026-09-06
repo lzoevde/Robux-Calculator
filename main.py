3ce8ea477cb42bdc393a03dcbc7e3bb693fd607c
lzoevde <kghsixlnw1234@gmail.com>
2026-09-06 21:48:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+intents.presences = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 bot_config = {"rbx1": 1000, "rbx2": 1000}
